a57a3761bbe7b982eca5f746c599a8d83f1bfeda
ZJ <1476253236@qq.com>
2020-04-22 10:44:14 +0800

    火狐驱动测试

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,13 +1,30 @@
 import com.zj0724.StepWebDriver.StepWebDriver;
 import com.zj0724.StepWebDriver.common.BaseStepWebDriver;
 import com.zj0724.StepWebDriver.common.ChromeStepWebDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebElement;
 
 public class Test {
 
     public static void main(String[] args) {
 
-//        StepWebDriver stepWebDriver = new ChromeStepWebDriver()
 
+        System.setProperty("webdriver.gecko.driver", "E:\\ZJ\\fireboxDriver\\geckodriver.exe");
+
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.addArguments("--start-maximized");
+//        firefoxOptions.addArguments("-headless");
+
+        WebDriver webDriver = new FirefoxDriver(firefoxOptions);
+
+        webDriver.get("https://www.baidu.com/");
+
+//        webDriver.quit();
+
+        WebElement webElement = new RemoteWebElement();
     }
 
 }
