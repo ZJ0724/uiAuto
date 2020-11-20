b799e8e4a05259672a97132bfda7fa50e817651f
ZJ <1476253236@qq.com>
2020-11-20 12:02:44 +0800

    切换至gradle构建

--- package.sh
+++ package.sh
@@ -1,33 +1,14 @@
 #!/bin/bash
 
-# 打包目录
-releasePath="release"
-# 包名
-packageName=""
-# maven打包存放目录
-buildPath="target/build"
-# 当前分支名
-branchName=$(git branch | grep "*")
+packagePath=".release"
+name="uiAuto"
+resourcePath="build"
 
-# 清除maven编译目录
-mvn clean
+./gradlew clean
+./gradlew build
+./gradlew buildResource
 
-# 根据分支名判断怎么进行打包
-if [ "${branchName}" == "* dev" ]; then
-    mvn package
-else
-    mvn package -P prod
-fi
+rm -rf ${packagePath}
+mkdir ${packagePath}
 
-# 获取包名
-for dirName in $(ls ${buildPath})
-do
-  packageName="${dirName}"
-done
-
-# 删除重新创建打包目录
-rm -rf ${releasePath}
-mkdir ${releasePath}
-
-# 打包
-tar -cvf ${releasePath}/${packageName}.zip -C ${buildPath} ${packageName}
+tar -cvf ${packagePath}/${name}.zip -C ${resourcePath} ${name}
